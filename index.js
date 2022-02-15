f511a3c8de0c5d0b71f3df22e0d7702c0e075858
KROKODYL4 <michal.krokodyl4@protonmail.com>
2022-02-15 11:46:15 +0100

    welcomescreen

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js')
+require("dotenv").config()
+
+const generateImage = require('./generateImage')
+
+const welcomeChannelId = "943084256945664010"
+
+const client  = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("messageCreate", (msg) => {
+    if (msg.content == "ping"){
+        msg.reply("Pong!")
+    }
+})
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Witaj na serwerze!`,
+        files: [img]
+    })
+})
+
+client.login(process.env.TOKEN)
